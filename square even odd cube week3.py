a479853af7be01ffc04361b5dd00714b45f84127
sayyedrabeeh <sayyedrabeeh240@gmail.com>
2024-08-17 18:00:40 +0530

    Add files via upload

---
+++ square even odd cube week3.py
@@ -0,0 +1,12 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+li=[1,2,3,4,5,6,7,8,9]
+
+p=[i**2 if i%2==0 else i**3 for i in li]
+print(p)
